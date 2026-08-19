a293a464aaa9cf7975ea2aaad7cd768c9d2d6fd2
suyog45gt <suyog45gt@gmail.com>
2026-08-19 23:44:39 +0545

    WAP to input 2 int numbers, a and b. Print True if a is greater than or equal to b. If not print False

---
+++ variables and data types/truefalse.py
@@ -0,0 +1,6 @@
+# WAP to input 2 int numbers, a and b.
+# Print True if a is greater than or equal to b. If not print False
+x=int(input("enter first number"))
+y=int(input("enter second number"))
+result = x>=y
+print(result)
