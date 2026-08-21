bdd41c978472d56bc807b06155793fc6cc4b07ee
suyog45gt <suyog45gt@gmail.com>
2026-08-21 08:57:19 +0545

    use of count method

---
+++ List & tuple/13.py
@@ -0,0 +1,5 @@
+# wap to count the number of students with the "A"grade in the following tuple.
+# [”C”, “D”, “A”, “A”, “B”, “B”, “A”]
+x=["C","D","A","A","B","B","A"]
+count=x.count("A")
+print(f"number of A grade = {count}")
