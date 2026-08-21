5a8e07652571f5dad190d430b7258c7086234100
suyog45gt <suyog45gt@gmail.com>
2026-08-21 20:03:06 +0545

    to store data in dictionary

---
+++ Dictionary & set/22.py
@@ -0,0 +1,7 @@
+# WAP to enter marks of 3 subjects from the user and store them in a dictionary. Start with an empty dictionary & add one by one. Use subject name as key & marks as value.
+marks ={}
+x=int("Enter a mark of physics")
+y=int("Enter a mark of chem")
+z=int("Enter a mark of math")
+marks.update({"physics": x,"chem": y,"math":z})
+print(marks)
