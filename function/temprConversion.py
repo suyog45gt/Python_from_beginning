9d9cc91785be9b0f212aacb6e2f4bafa56e7ff22
suyog45gt <suyog45gt@gmail.com>
2026-08-25 21:19:20 +0545

    to convert celsius to fahrenheight

---
+++ function/temprConversion.py
@@ -0,0 +1,5 @@
+# to convert celsius to fahrenheight
+def conversion():
+    c=int(input("enter temperature in celsious"))
+    return (c * 9/5) + 32 
+print("temperature in fahrenheihtt =",conversion())
