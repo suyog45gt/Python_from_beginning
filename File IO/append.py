96cf1b75ccd1b62124480017371bfb4c8721014e
suyog45gt <suyog45gt@gmail.com>
2026-08-26 19:00:32 +0545

    Python program that asks the user to enter their name and age, saves the information to students.txt, and then allows new student information to be appended without deleting existing data.

---
+++ File IO/append.py
@@ -0,0 +1,6 @@
+# Write a Python program that asks the user to enter their name and age, saves the information to students.txt, and then allows new student information to be appended without deleting existing data.
+with open("info,txt","a") as f:
+    x=input("enter a name")
+    y=input("enter a age")
+    list=[x,y]
+    f.write(str(list)+"\n")
